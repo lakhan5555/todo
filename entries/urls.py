71d69bf37d005fa57e530ad276024d67f93de260
Ram Lakhan Singh <62960598+lakhan5555@users.noreply.github.com>
2021-03-18 15:37:49 +0530

    Update urls.py

--- entries/urls.py
+++ entries/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('',views.loginPage, name="login"),
     path('home/',views.home,name='home'),
     path('add/<str:user_name>', views.add, name="add"),
     path('edit/<int:pk>', views.edit, name="edit"),
     path('delete/<int:pk>', views.delete, name="delete"),
-    path('',views.loginPage, name="login"),
     path('register/', views.registerPage, name="register"),
     path('logout/', views.LogoutUser, name="logout"),
 ]
